aef58afbcb50b5df4abb056186c5e4d1498930a2
JsonRest <jsliamzon@gmail.com>
2022-02-22 15:16:55 +0800

    Update deploy-infra.sh
    Changed the domain name

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -12,7 +12,7 @@ STACK_NAME=awsbootstrap
 REGION=us-east-1 
 CLI_PROFILE=awsbootstrap
 EC2_INSTANCE_TYPE=t2.micro 
-DOMAIN=educative-cloud-course.com
+DOMAIN=pgcapstone.com
 
 GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
 GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
